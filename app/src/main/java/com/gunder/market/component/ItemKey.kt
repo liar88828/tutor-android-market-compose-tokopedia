88ce9bc37f18aab36d7c0f0582e07b3e01facb8b
brian <liar88828@gmail.com>
2024-07-14 19:27:31 +0700

    save STATE HOSTING

--- app/src/main/java/com/gunder/market/component/ItemKey.kt
+++ app/src/main/java/com/gunder/market/component/ItemKey.kt
@@ -1,2 +1,45 @@
 package com.gunder.market.component
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.gunder.market.ui.theme.MarketTheme
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+	var count by remember {
+		mutableIntStateOf(0)
+	}
+
+	fun increment(): Unit {
+		count++
+	}
+	StateFulComponent(count, onChange = { increment() })
+
+}
+
+@Composable
+fun StateFulComponent(count: Int, onChange: () -> Unit) {
+	Column {
+		Text(text = "count number $count")
+		Button(onClick = onChange) {
+			Text(text = "Tambah Data")
+		}
+	}
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MainSateFulComponent() {
+	MarketTheme {
+		MainScreen()
+	}
+
+}
